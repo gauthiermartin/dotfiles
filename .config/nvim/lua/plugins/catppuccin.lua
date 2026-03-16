f0d3a463ebc3935a20629ad01df3728e3ca9391b
Martin Gauthier <gauthiermartin86@gmail.com>
2026-03-16 08:24:31 -0400

    🔧 chore: sync dotfiles with live config
    - expand .zsh_aliases: add .., re-source, ll, infra aliases; update ls to eza -la
    - remove deprecated catppuccin bufferline workaround (upstreamed fix)

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -6,12 +6,7 @@ return {
     -- opts = {
     --   flavour = "frappe",
     -- },
-    -- Remove this once the next version of catppuccin is released and the get is fixed
     opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
       opts.flavour = "mocha"
       return opts
     end,
